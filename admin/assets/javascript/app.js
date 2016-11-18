c8473020c3f93cf09ade6d2a7b4166268a1ca08e
Allan Guggenheim <allan.guggenheim@gmail.com>
2016-11-17 21:22:21 -0800

    adding admin and map working

---
+++ admin/assets/javascript/app.js
@@ -0,0 +1,103 @@
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAVJTf6lXY2B4y1-ErbYIv92pRYoLQywLQ",
+    authDomain: "study-group-d774f.firebaseapp.com",
+    databaseURL: "https://study-group-d774f.firebaseio.com",
+    storageBucket: "study-group-d774f.appspot.com",
+    messagingSenderId: "128568248931"
+  };
+  firebase.initializeApp(config);
+
+	// Referencing the firebase
+	var database= firebase.database();
+	var students = database.ref();
+
+
+function deletestudent(studentKey) {
+  console.log(students);
+  students.child(studentKey).remove().then(function() {
+    // Code after remove
+  });
+	//Prevents moving to new page
+	return false;
+};
+
+
+$(document).ready(function(){ 
+
+
+
+	//Initial values
+	var student = {
+		Name: "",
+		Dest: ""
+	};
+		//Button for adding students
+	$("#submitStudent").on("click", function(){
+		// alert("yoo");
+
+		//Getting new student input
+		student.Name = $('#addName').val().trim();
+		student.Dest = $('#addDestination').val().trim();
+
+		//Creating temporary object for holding student new
+
+		var newstudent = {
+			name: student.Name,
+			destination: student.Dest
+		}
+
+		//Uploads student data to the database
+		database.ref().push({
+			newstudent,
+			dateAdded: firebase.database.ServerValue.TIMESTAMP
+		});
+
+		// Clears all of the text-boxes
+		$("#addName").val("");
+		$("#addDestination").val("");
+
+		//Prevents moving to new page
+		return false;
+
+	});
+
+	//Make all of this update every 60 seconds
+
+		database.ref().on("child_removed", function(childSnapshot){
+			location.reload();
+		});
+
+	//Create Firebase event for adding students to the database and a row in the html with entries
+		database.ref().on("child_added", function(childSnapshot){
+			//console.log(childSnapshot.val());
+
+		//Add stored students to the schedule
+			//assigning stored student names to a variable
+			student.Name = childSnapshot.val().newstudent.name;
+				$('#name').append('<div class="row" id="nameRow">');
+				$('#nameRow').append(student.Name);
+				//Add a new line for the next student
+				$('#nameRow').append("<br/>");
+
+			//Destination
+			student.Dest = childSnapshot.val().newstudent.destination;
+				$('#destination').append('<div class="row" id="destRow">');
+				$('#destRow').append(student.Dest);
+				//Add a new line for the next student
+				$('#destRow').append("<br/>");
+
+
+				$('#delete').append('<div class="row" id="deleteRow">');
+				$('#deleteRow').append('<button id=\"deletestudent\" type=\"submit\" class=\"btn btn-primary\" onclick=\"deletestudent(\''+childSnapshot.key+'\');\">Delete</button>');
+				// $('#deleteRow').append('<button id=\"deletestudent\" type=\"submit\" class=\"btn btn-primary\" onclick=\"deletestudent(\''+student.Name+'\');\">Delete</button>');
+				$('#deleteRow').append("<br/>");
+				// If any errors are experienced, log them to console.
+			}, function (errorObject) {
+
+			  	console.log("The read failed: " + errorObject.code);
+	});
+
+
+
+});	
---
+++ js/modal.js
@@ -0,0 +1,31 @@
+
+
+$(document).ready(function(){
+
+var placecategory;
+var placezipcode;
+
+function something() {
+
+}
+
+// Check if any button is clicked forever
+$('button').on("click", function() {
+
+
+//CHARACTER BUTTON SCOPE
+    // Now that a button has been clicked, let's check if it's a character
+    if ($(this).hasClass("yelpgoogle")){
+        // placecategory = $('#placecategory').val().trim();
+        placezipcode = $('#placezipcode').val().trim();
+        window.location.href = "yelp_google.html?placecategory=" + placecategory + "&placezipcode=" + placezipcode;
+    }
+   	
+    if ($(this).hasClass("adminbutton")){
+        // placecategory = $('#placecategory').val().trim();
+        window.location.href = "admin/index.html";
+    }
+
+})
+
+})
